d9b7f64844ef493b44ce093768cfc4502a2e775a
JosephVuoto <joseph.vuoto@gmail.com>
2019-09-20 20:07:17 +1000

    update

--- src/main/java/com/spm/carwash/controller/DefaultController.java
+++ src/main/java/com/spm/carwash/controller/DefaultController.java
@@ -2,6 +2,10 @@ package com.spm.carwash.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 /**
  * @author Yangzhe Xie
@@ -12,5 +16,16 @@ public class DefaultController {
     @GetMapping("/login")
     public String login() {
         return "/login";
+    }
+
+    @GetMapping("/user")
+    public String userInfo() {
+        return "/user";
+    }
+
+    @RequestMapping("/profile")
+    @ResponseBody
+    public Principal profile(Principal principal){
+        return principal;
     }
 }
